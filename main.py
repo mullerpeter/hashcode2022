d0f41dd04ec3acb7daa5e8661dc86fddba6a126b
Peter Müller <6986867+mullerpeter@users.noreply.github.com>
2022-02-24 21:35:18 +0100

    esfjkfjk

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 from dataparser import Data
 from optimizer import run_optimisation
 from score import score
-from solver import solve
+from solver import solve, solve_peter
 from zip_code import zip_code
 
 if __name__ == '__main__':
 
     task_name = 'a_an_example'
-    # task_name = 'b_better_start_small'
-    # task_name = 'c_coarse'
-    # task_name = 'd_difficult'
-    # task_name = 'e_elaborate'
-
-    load_best = False
-
     data = Data(task_name)
-
+    data.solution = solve_peter(data)
+    data.write_solution()
+    task_name = 'b_better_start_small'
+    data = Data(task_name)
+    data.solution = solve_peter(data)
+    data.write_solution()
+    task_name = 'c_collaboration'
+    data = Data(task_name)
+    data.solution = solve_peter(data)
+    data.write_solution()
+    task_name = 'd_dense_schedule'
+    data = Data(task_name)
+    data.solution = solve_peter(data)
+    data.write_solution()
+    task_name = 'e_exceptional_skills'
+    data = Data(task_name)
+    data.solution = solve_peter(data)
+    data.write_solution()
+    task_name = 'f_find_great_mentors'
+    data = Data(task_name)
+    data.solution = solve_peter(data)
+    data.write_solution()
 
     zip_code()
+
+
 
