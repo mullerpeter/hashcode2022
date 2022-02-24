ea275050a56fe729288f346e5df31a4f3f869da9
Peter Müller <6986867+mullerpeter@users.noreply.github.com>
2022-02-24 19:12:18 +0100

    add data parser

--- main.py
+++ main.py
@@ -6,23 +6,16 @@ from zip_code import zip_code
 
 if __name__ == '__main__':
 
-    # task_name = 'a_an_example'
-    # task_name = 'b_basic'
+    task_name = 'a_an_example'
+    # task_name = 'b_better_start_small'
     # task_name = 'c_coarse'
-    task_name = 'd_difficult'
+    # task_name = 'd_difficult'
     # task_name = 'e_elaborate'
 
-    load_best = True
+    load_best = False
 
     data = Data(task_name)
-    if load_best:
-        data.load_best_solution()
-    else:
-        solve(data)
-    task_score = score(data)
-    data.write_if_best(task_score)
 
-    run_optimisation(data, num_steps=10)
 
     zip_code()
 
